9eac3babfef7cc49c4501b7e7023097df590e825
Romans Malinovskis <romans@agiletoolkit.org>
2019-11-23 11:20:01 +0000

    wip

--- root/agiletoolkit.org/github.tf
+++ root/agiletoolkit.org/github.tf
@@ -1,3 +1,27 @@
 provider "github" {
   organization = "atk4"
 }
+
+locals {
+  github_owners = [
+    "romaninsh",
+    "ibelar",
+    "DarkSide666",
+    "KraKraDooh"
+  ]
+  github_maintainers = [
+    "abbadon1334",
+    "acicovic",
+    "PhilippGrashoff",
+  ]
+  collaborators = [
+    "gowrav-vishwakarma",
+    "jancha",
+    "skondakov"
+  ]
+}
+
+resource "github_membership" "atk4_owners" {
+  for_each = toset(local.github_owners)
+  username = each.value
+}
