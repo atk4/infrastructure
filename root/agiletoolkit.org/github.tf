e825ac0d4c654b058d8ceb541e5c499936deac58
Romans Malinovskis <romans@agiletoolkit.org>
2019-11-23 11:30:43 +0000

    wip

--- root/agiletoolkit.org/github.tf
+++ root/agiletoolkit.org/github.tf
@@ -26,3 +26,14 @@ resource "github_membership" "atk4_owners" {
   username = each.value
   role = "member"
 }
+
+resource "github_team" "atk4_maintainers" {
+  name = "Agile Toolkit Maintainers"
+  description = "Approval rights for all ATK repositories"
+}
+
+
+resource "github_team" "atk4_contributors" {
+  name = "Agile Toolkit Regular Contributor"
+  description = "Creating branches and pull requests in ATK repositories"
+}
