4b298befacdc8c2216c4f91abee2496757516749
Romans Malinovskis <romans@agiletoolkit.org>
2019-12-16 22:20:30 +0000

    wip

--- root/workspaces.tf
+++ root/workspaces.tf
@@ -6,16 +6,28 @@ variable "DIGITALOCEAN_TOKEN" {}
 
 module "agiletoolkit_org_github" {
   source = "./workspace"
-  name = "agiletoolkit_org_github"
+  name = "atk4-github"
   path = "projects/agiletoolkit.org/github"
   github_oauth = var.GITHUB_OAUTH
   tfe_org = var.TFE_ORG
 
   env = {
-    DIGITALOCEAN_TOKEN: var.DIGITALOCEAN_TOKEN
     GITHUB_TOKEN: var.GITHUB_TOKEN
   }
 
 }
 
+/*
+module "agiletoolkit_org_github" {
+  source = "./workspace"
+  name = "agiletoolkit_org_digitalocean"
+  path = "projects/agiletoolkit.org/digitalocean"
+  github_oauth = var.GITHUB_OAUTH
+  tfe_org = var.TFE_ORG
 
+  env = {
+    DIGITALOCEAN_TOKEN: var.DIGITALOCEAN_TOKEN
+  }
+
+}
+*/
