1f56eb588b34aee38d12ff9302b3613cce4ca4a8
Romans Malinovskis <romans@agiletoolkit.org>
2020-02-24 22:47:18 +0000

    cleanups

--- projects/agiletoolkit.org/github/agiletoolkit.org/addon/addon-repo.tf
+++ projects/agiletoolkit.org/github/agiletoolkit.org/addon/addon-repo.tf
@@ -1,5 +1,5 @@
-variable "name" { }
-variable "description" { }
+variable "name" {}
+variable "description" {}
 variable "topics" {
   default = []
 }
@@ -7,39 +7,39 @@ variable "topics" {
 variable "access" {
   type = object({
     owner_team_id = string
-    maintainers = list(string)
-    contributors = list(string)
+    maintainers   = list(string)
+    contributors  = list(string)
   })
 }
 
 locals {
   labels = {
-    "PHP7.3+": "FF0000"
+    "PHP7.3+" : { c: "FF0000", d: "This can only run on PHP 7.3 and higher" }
   }
 }
 
 variable "has_projects" {
-  type = bool
+  type    = bool
   default = false
 }
 
 variable "has_wiki" {
-  type = bool
+  type    = bool
   default = false
 }
 
 variable "protect_develop" {
-  type = bool
+  type    = bool
   default = true
 }
 
 variable "protect_master" {
-  type = bool
+  type    = bool
   default = true
 }
 
 resource "github_repository" "addon" {
-  name = var.name
+  name        = var.name
   description = var.description
   lifecycle {
     prevent_destroy = true
@@ -49,38 +49,42 @@ resource "github_repository" "addon" {
   allow_rebase_merge = false
 
 
-  homepage_url = "https://agiletoolkit.org/"
-  has_wiki = var.has_projects
-  has_projects = var.has_projects
-  has_issues = true
+  homepage_url  = "https://agiletoolkit.org/"
+  has_wiki      = var.has_projects
+  has_projects  = var.has_projects
+  has_issues    = true
   has_downloads = false
-  topics = concat(["agile","atk4","php"], var.topics)
+  topics        = concat(["agile", "atk4", "php"], var.topics)
 }
 
 resource "github_issue_label" "test_repo" {
-  for_each = toset(local.labels)
+  for_each   = local.labels
   repository = github_repository.addon.name
   name       = each.key
-  color      = each.value
+  color      = each.value.c
+  description= each.value.d
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "github_team_repository" "owner_team" {
   repository = github_repository.addon.name
-  team_id = var.access.owner_team_id
+  team_id    = var.access.owner_team_id
   permission = "admin"
 }
 
 resource "github_repository_collaborator" "maintainer" {
-  for_each = toset(var.access.maintainers)
+  for_each   = toset(var.access.maintainers)
   repository = github_repository.addon.name
-  username = each.value
+  username   = each.value
   permission = "admin"
 }
 
 resource "github_repository_collaborator" "contributor" {
-  for_each = toset(var.access.contributors)
+  for_each   = toset(var.access.contributors)
   repository = github_repository.addon.name
-  username = each.value
+  username   = each.value
   permission = "push"
 }
 
@@ -89,7 +93,7 @@ resource "github_repository_collaborator" "contributor" {
 resource "github_branch_protection" "addon-develop" {
   count = var.protect_develop ? 1 : 0
 
-  branch = "develop"
+  branch     = "develop"
   repository = github_repository.addon.name
   required_status_checks {}
   required_pull_request_reviews {
@@ -100,6 +104,6 @@ resource "github_branch_protection" "addon-develop" {
 resource "github_branch_protection" "addon-master" {
   count = var.protect_master ? 1 : 0
 
-  branch = "master"
+  branch     = "master"
   repository = github_repository.addon.name
 }
