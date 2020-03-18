667399b70abee5509b1deaf783e670faf2e780ab
Romans Malinovskis <romans@agiletoolkit.org>
2020-03-18 13:19:21 +0000

    wip

--- projects/agiletoolkit.org/kube/provider.tf
+++ projects/agiletoolkit.org/kube/provider.tf
@@ -5,6 +5,7 @@ variable "DIGITALOCEAN_TOKEN" {}
 
 
 provider "kubernetes" {
+  load_config_file = false
   host  = var.KUBE_HOST
   token = var.KUBE_TOKEN
   cluster_ca_certificate = base64decode(var.KUBE_CERT)
