ccc43e71b9aa8d2831f3ef04ff056eacf94a5803
Romans Malinovskis <romans@agiletoolkit.org>
2021-01-03 17:59:52 +0000

    wip

--- projects/agiletoolkit.org/kube/provider.tf
+++ projects/agiletoolkit.org/kube/provider.tf
@@ -13,11 +13,8 @@ provider "kubernetes" {
 
 provider helm {
   kubernetes {
-    load_config_file = false
-
     host  = var.KUBE_HOST
     token = var.KUBE_TOKEN
     cluster_ca_certificate = base64decode(var.KUBE_CERT)
-
   }
 }
